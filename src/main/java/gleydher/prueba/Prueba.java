5e925b0b45894046f41aa7253e4d5393775d432a
hp <hp@192.168.56.1>
2025-10-23 00:35:41 -0500

    Mi_primer_Commit_Proyecto_Java

---
+++ src/main/java/gleydher/prueba/Prueba.java
@@ -0,0 +1,21 @@
+
+package gleydher.prueba;
+
+
+import javax.swing.JOptionPane;
+
+public class Prueba {
+    
+    public static void main(String[] args) {
+    
+ 
+       String dato =  JOptionPane.showInputDialog(null, "Ingrese su dato ");
+     
+       
+       JOptionPane.showMessageDialog(null,"El dato ingresado es: " + dato);
+      
+        
+
+    }
+    
+}
