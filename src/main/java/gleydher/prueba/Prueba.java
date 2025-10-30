83db0f0ed68edad93b0e903de43edbc2c8bd8fa4
hp <hp@192.168.56.1>
2025-10-30 02:08:47 -0500

    Mi_primer_Commit_Proyecto_Java

--- src/main/java/gleydher/prueba/Prueba.java
+++ src/main/java/gleydher/prueba/Prueba.java
@@ -1,20 +1,36 @@
 
 package gleydher.prueba;
 
-
-import javax.swing.JOptionPane;
-
 public class Prueba {
     
-    public static void main(String[] args) {
-    
+ public static void main (String [] args) {
+// definición de cada una de las variables
+int a = 20, b = 10, c = 0, d = 20, e = 40, f =30;
+    boolean condition = true;
+System.out.println("Valor de a inicial = "+ a);
+
+ // operador de pre-incremento
+ // a = a+1 y entonces c = a;
+ c = ++a;
+System.out.println("Valor de c preincremento(++a) = " + c);
+
+ // operador de post-incremento
+ // c=b entonces b=b+1 (b pasa a ser 11)
+ c = b++;
+ System.out.println("Valor de c postincremento(b++) = " + c);
  
-       String dato =  JOptionPane.showInputDialog(null, "Ingrese su dato ");
-     
-       
-       JOptionPane.showMessageDialog(null,"El dato ingresado es: " + dato);
-      
-        
+ // operador de pre-decremento
+ // d=d-1 entonces c=d
+ c = --d;
+System.out.println("Valor de c predecremento (--d) = " + c);
+
+ // operador de post-decremento
+ // c=e entonces e=e-1 (e pasa aser 39)
+ c = e--;
+System.out.println("Valor de c postdecremento(e--) = " + c);
+ // Operador lógico not
+ 
+System.out.println("Valor de !condition =" + !condition);
 
     }
     
