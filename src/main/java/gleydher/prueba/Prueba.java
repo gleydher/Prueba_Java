1b89a9dc6baac533eecf53989932ab9e45ae6c2a
hp <hp@192.168.56.1>
2025-11-05 22:53:30 -0500

    Mi_Tercer_Commit

--- src/main/java/gleydher/prueba/Prueba.java
+++ src/main/java/gleydher/prueba/Prueba.java
@@ -3,35 +3,19 @@ package gleydher.prueba;
 
 public class Prueba {
     
- public static void main (String [] args) {
-// definición de cada una de las variables
-int a = 20, b = 10, c = 0, d = 20, e = 40;
-    boolean condition = true;
-System.out.println("Valor de a inicial = "+ a);
-
- // operador de pre-incremento
- // a = a+1 y entonces c = a;
- c = ++a;
-System.out.println("Valor de c preincremento(++a) = " + c);
-
- // operador de post-incremento
- // c=b entonces b=b+1 (b pasa a ser 11)
- c = b++;
- System.out.println("Valor de c postincremento(b++) = " + c);
- 
- // operador de pre-decremento
- // d=d-1 entonces c=d
- c = --d;
-System.out.println("Valor de c predecremento (--d) = " + c);
-
- // operador de post-decremento
- // c=e entonces e=e-1 (e pasa aser 39)
- c = e--;
-System.out.println("Valor de c postdecremento(e--) = " + c);
- // Operador lógico not
- 
-System.out.println("Valor de !condition =" + !condition);
-
-    }
-    
-}
+public static void main(String[] args) {
+int a = 20, b = 10;
+String x = "Operador", y ="Operador";
+int ar[] = { 1, 2, 3 }; int br[] = { 1, 2, 3 };
+ boolean condicion = true;
+ //varios operadores condicionales
+ System.out.println("a == b :" + (a ==b));
+ System.out.println("a < b :" + (a < b));
+ System.out.println("a <= b :" + (a <=b));
+ System.out.println("a > b :" + (a > b));
+ System.out.println("a >= b :" + (a >=b));
+ System.out.println("a != b :" + (a != b));
+ // Los Arrays no se pueden comparar con // operadores relacionales porque los objetos 
+// almacenan referencias, mas no el valor
+ System.out.println("x == y : " + (ar ==br));
+System.out.println ("condicion==true :" +(condicion == true)); } }
